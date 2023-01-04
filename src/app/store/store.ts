d1546f1e43b66561e37147ef6f806c0e4d306827
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-01-04 21:39:45 +0600

    Getting started with RTK query. Data retrieval requests (query)

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,15 +1,23 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import userReducer from "./reducers/UserSlice"
+import { postAPI } from "./../services/PostService";
+import {
+    combineReducers,
+    configureStore,
+    getDefaultMiddleware
+} from "@reduxjs/toolkit";
+import userReducer from "./reducers/UserSlice";
 
 const rootReducer = combineReducers({
-    userReducer
+    userReducer,
+    [postAPI.reducerPath]: postAPI.reducer
 });
 
 export const setupStore = () => {
     return configureStore({
-        reducer: rootReducer
-    })
-}
+        reducer: rootReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware().concat(postAPI.middleware)
+    });
+};
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
